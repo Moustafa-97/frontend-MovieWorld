5577bd5236333e36a90a14b1fa4f822fb2218c00
Moustafa <moustafaadel097@gmail.com>
2024-03-05 22:06:01 +0200

    movie

--- src/Pages/theme/theme.tsx
+++ src/Pages/theme/theme.tsx
@@ -1,9 +1,37 @@
 import { PaletteMode } from "@mui/material";
 
-import { grey } from "@mui/material/colors";
+import { amber, deepOrange, grey, red } from "@mui/material/colors";
+
+const mode = localStorage.getItem("mymode");
+console.log(mode);
 
 const theme = {
-  palette: {},
+  palette: {
+    ...(mode === 'light'
+            ? {
+                // palette values for light mode
+                primary: amber,
+                divider: amber[200],
+                
+                text: {
+                  primary: grey[900],
+                  secondary: grey[800],
+                },
+              }
+            : {
+                // palette values for dark mode
+                primary: grey,
+                divider: deepOrange[700],
+                background: {
+                  default: grey[900],
+                  paper: grey[900],
+                },
+                text: {
+                  primary: '#fff',
+                  secondary: grey[500],
+                },
+              }),
+  },
 };
 
 export default theme;
--- src/Pages/theme/use-color-theme.tsx
+++ src/Pages/theme/use-color-theme.tsx
@@ -1,6 +1,7 @@
 import { useState, useMemo, useEffect } from "react";
 import { createTheme, PaletteMode } from "@mui/material";
 import theme from "./theme";
+import { amber, blue, deepOrange, grey } from "@mui/material/colors";
 
 export const useColorTheme = () => {
   const mymode: String | null | any = localStorage.getItem("mymode");
@@ -20,8 +21,36 @@ export const useColorTheme = () => {
       createTheme({
         ...theme,
         palette: {
-          ...theme.palette,
           mode,
+          ...(mode === 'light'
+          ? {
+              // palette values for light mode
+              primary: amber,
+              divider: amber[200],
+              background: {
+                default: blue[900],
+                paper: blue[900],
+              },
+              text: {
+                primary: amber[900],
+                secondary: amber[800],
+              },
+            }
+          : {
+              // palette values for dark mode
+              primary: grey,
+              divider: deepOrange[700],
+              background: {
+                default: grey[900],
+                paper: grey[900],
+              },
+              text: {
+                primary: '#fff',
+                secondary: grey[500],
+              },
+            }),
+              // mode,
+          
         },
       }),
     [mode]
