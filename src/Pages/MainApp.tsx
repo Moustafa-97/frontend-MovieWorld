a35d3f541c2ade623c6cef229cb96dbea18be8db
Moustafa <moustafaadel097@gmail.com>
2024-03-14 20:18:26 +0200

    movie

--- src/Pages/MainApp.tsx
+++ src/Pages/MainApp.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./Login-Signup/Login";
